4d298d4c37e82c7e180398c6d2aa388ef08922f6
Abigail <abigail@dogbuilt.net>
2023-04-14 00:38:27 -0400

    Remove pseudo command

--- dozer/cogs/development.py
+++ dozer/cogs/development.py
@@ -105,25 +105,25 @@ class Development(Cog):
     `{prefix}eval await ctx.send('Hello world!')` - send "Hello World!" to this channel
     """
 
-    @command(name='su', pass_context=True)
-    async def pseudo(self, ctx: DozerContext, user: discord.Member, *, command: str):
-        """Execute a command as another user."""
-        msg = copy.copy(ctx.message)
-        msg.author = user
-        msg.content = command
-        context = await self.bot.get_context(msg)
-        context.is_pseudo = True  # adds new flag to bypass ratelimit
-        # let's also add a log of who ran pseudo
-        logger.info(
-            f"Running pseudo on request of {ctx.author} ({ctx.author.id}) in '{ctx.guild}' #{ctx.channel}:")
-        logger.info("-" * 32)
-        logger.info(ctx.message.content)
-        logger.info("-" * 32)
-        await self.bot.invoke(context)
+    # @command(name='su', pass_context=True)
+    # async def pseudo(self, ctx: DozerContext, user: discord.Member, *, command: str):
+    #     """Execute a command as another user."""
+    #     msg = copy.copy(ctx.message)
+    #     msg.author = user
+    #     msg.content = command
+    #     context = await self.bot.get_context(msg)
+    #     context.is_pseudo = True  # adds new flag to bypass ratelimit
+    #     # let's also add a log of who ran pseudo
+    #     logger.info(
+    #         f"Running pseudo on request of {ctx.author} ({ctx.author.id}) in '{ctx.guild}' #{ctx.channel}:")
+    #     logger.info("-" * 32)
+    #     logger.info(ctx.message.content)
+    #     logger.info("-" * 32)
+    #     await self.bot.invoke(context)
 
-    pseudo.example_usage = """
-    `{prefix}su cooldude#1234 {prefix}ping` - simulate cooldude sending `{prefix}ping`
-    """
+    # pseudo.example_usage = """
+    # `{prefix}su cooldude#1234 {prefix}ping` - simulate cooldude sending `{prefix}ping`
+    # """
 
 
 def load_function(code: str, globals_, locals_):
